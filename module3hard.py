dc05e3f970c546efc9caa4669313f724d0c7efe7
MisterCbing <150890001+MisterCbing@users.noreply.github.com>
2024-05-30 22:12:20 +0400

    Add files via upload

---
+++ module3hard.py
@@ -0,0 +1,36 @@
+def super_sum(el):
+    if isinstance(el, int) or isinstance(el, float):
+        return el
+    elif isinstance(el, str):
+        return len(el)
+    elif isinstance(el, list) or isinstance(el, tuple) :
+        if len(el) < 1:
+            return 0
+        else:
+            return super_sum(el[0]) + super_sum(el[1:])
+    elif isinstance(el, dict):
+        temp = [[x, y] for x, y in el.items()]
+        return super_sum(temp)
+    elif isinstance(el, set):
+        temp = 0
+        for i in el:
+            return super_sum(i)
+    else:
+        return 0
+
+def calculate_structure_sum(st):
+    res = 0
+    for i in st:
+        res += super_sum(i)
+    return res
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
